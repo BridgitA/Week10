f5c5004e1e1807ab6f022ed9bf6a2a213a4becad
Bridgit Arika <bridgitmob@gmail.com>
2020-09-02 16:07:40 -0500

    completed mod 4 code assignment

--- mod4.py
+++ mod4.py
@@ -1,3 +1,15 @@
 def str_analysis(string):
     while string.lower() == "":
         string = input("Enter a word or interger: ")
+    else: 
+        if string.lower().isdigit(): 
+            if int(string) > 100:
+                print("This is a big number")
+            elif int(string) < 100:
+                print("This is a smaller number than expected")
+        elif string.lower().isalpha():
+            print("This string is all alphabetic")
+        else:
+            print("This string contains multiple character types")
+str_analysis(input("Enter a word or interger: "))                        
+
